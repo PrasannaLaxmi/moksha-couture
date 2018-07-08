c695362cf4c36c8f644d3bd31c144564073a2c82
prasanna <prasannalaxmiattuluri@gmail.com>
2018-07-08 17:56:16 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,139 +1,30 @@
 <?php
-include"includes/db.php";
-$flag=0;
-$sql="select * from products where product_priority=High";
-$query = mysql_query($sql);
-while($row=mysql_fetch_array($query)){
-    $pro_img1 = $row['product_img1'];
+session_start();
+error_reporting('0');
+if(!empty($_SESSION['username']))
+{
+   
+?>
+<html>
+<head>
+<title>Admin Panel</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php 
+include 'inc/header.php';
+include 'inc/body_left.php';
+include 'inc/body_right.php';
+include 'inc/footer.php';
+?>
+</body>
+</html>
+<?php 
 }
-$sql="select * from products where product_priority='High'";
-$query = mysql_query($sql);
-while($row=mysql_fetch_array($query)){
-    $pro_img2 = $row['product_img1'];
-}
-$sql="select * from products where product_priority='High'";
-$query = mysql_query($sql);
-while($row=mysql_fetch_array($query)){
-    $pro_img3 = $row['product_img1'];
-}
-
-$sql="select * from users";
-$run=mysql_query($sql);
-while($row_pro=mysql_fetch_array($run)){
-    $name = $row_pro['uname'];
-
+else 
+{
+    include 'loginform.php';
 }
 ?>
-
-<?php include "includes/header.php"; ?>
-    <!-- Header Component -->
-
-    <section>
-        <div id="carouselFade" class="carousel slide carousel-fade" data-ride="carousel">
-
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <div class="carousel-caption">
-                        <a type="button" class="btn btn-primary pull-right" href="products.php" id="shop_btn">SHOP NOW</a>
-
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="carousel-caption">
-
-                        <a type="button" class="btn btn-primary pull-right" href="products.php" id="shop_btn">SHOP NOW</a>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="carousel-caption">
-
-                        <a type="button" class="btn btn-primary pull-right" href="products.php" id="shop_btn">SHOP NOW</a>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carouselFade" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true" style="left:15px;"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#carouselFade" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true" style="right:15px;"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-
-    </section>
-    <section class="spacer2">
-        <div class="container">
-            <h1 class="section-header">Welcome to Moksha</h1>
-        </div>
-        <div class="container fadeInUp">
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-1 col-xs-12">
-                    <div class="panel panel-default about-panel shadow">
-                        <div class="panel-body">
-                               <p>
-                                Moksha Couture Line of clothing is all about elegance in traditional Indian wear.
-                                It is an adorable collection of fresh designs with a hint of classics.
-
-                            </p>
-                            <p>
-                                We at Moksha, work on customized orders for Bridal Sarees, Engagement Lehengas, Anarkalis and sarees for your special occasion.
-                                Embracing your elegance since 2012, we give you the most memorable and lovely experience in creating your perfect look on your special day.
-                            </p>
-                            <button class="btn btn-primary" ><a style="color:white;text-decoration: none;" href="about.php">Read More</a></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="spacer2">
-        <div class="container banner">
-            <h1 class="section-header">Trendings</h1>
-        </div>
-        <div class="container banner-block fadeInUp">
-            <div class="row">
-                <ul class="trendings">
-                    <li class="col-sm-4">
-                        <div class="trend-box">
-                            <?php echo "<img src='admin_area/product_images/$pro_img1' class='img-responsive img-thumbnail'>"; ?>
-                        </div>
-                    </li>
-                    <li class="col-sm-4">
-                        <div class="trend-box">
-                            <?php echo "<img src='admin_area/product_images/$pro_img2' class='img-responsive img-thumbnail'>"; ?>
-                        </div>
-                    </li>
-                    <li class="col-sm-4">
-                        <div class="trend-box">
-                            <?php echo "<img src='admin_area/product_images/$pro_img3' class='img-responsive img-thumbnail'>"; ?>
-                        </div>
-                    </li>
-                </ul>
-                <button class="btn btn-primary pull-right" ><a style="color:white;text-decoration: none;" href="products.php">More.</a></button>
-            </div>
-            <div class="clearfix"></div>
-        </div>
-    </section>
-    <!--<section class="spacer5">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <address>
-                        <strong>Twitter, Inc.</strong><br>
-                        1355 Market Street, Suite 900<br>
-                        San Francisco, CA 94103<br>
-                        <abbr title="Phone">P:</abbr> (123) 456-7890
-                    </address>
-                </div>
-                <div class="col-sm-8">
-                    <div id="googleMap" style="width:100%;height:400px;"></div>
-                </div>
-            </div>
-        </div>
-    </section>-->
-   <?php include "includes/footer.php"; ?>
-    <!-- Ends -->
--- products.php
+++ products.php
@@ -1,227 +1,279 @@
-<?php
-$flag = 1;
+<div id="bodyright">
+<script src="js/jscolor.js"></script>
+<link rel="stylesheet" href="bootstrap.min.css">
+<script src="jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script type="text/javascript">
+
+</script>
+<div class="row">
+<h3 style="color:black;margin-top:1.3%">Add New Products From Here</h3>
+<form method="post" enctype="multipart/form-data">
+<div class="col-sm-3 add_product">
+  <label style="margin-left: 3%;padding-bottom:1px;">Enter Product Name Here </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Enter Product Id Here </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Enter Product Description </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Select Category Name </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Select MidCategory Name </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Select SubCategory Name </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Select Product Image1 </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Select Product Image2 </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Select Product Image3 </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Select Product Image4 </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Select Product Image5</label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Enter Product Keywords </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Enter Product Color </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Enter Product Size </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Enter Product Price </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Enter Product Discount </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Select Stcock Availability </label><br/>
+  <label style="margin-left: 3%;padding-bottom:1px;">Select Product Priroity </label><br/>
+
+</div>
+<div class="col-md-6 add_pro_input">
+<label style=""><input style="width: 35em;" class="form-control" type="text" name="product_name" required></label><br/>
+
+<label style=""><input class="form-control" style="width: 35em;" type="text" name="product_id" required></label><br/>
+
+<label style=""><input style="width: 35em;" class="form-control" type="text" name="product_description"></label><br/>
+
+<label class="sel_cat_name" style=""><select style="width: 35em;" class="form-control" class="form-control" name="main_cat" id="category">
+<option>Select Category</option>
+<?php 
+include 'inc/function.php'; echo view_all_categories();
 ?>
+</select></label><br>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Moksha Couture</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-    <link href="css/sidebar.css" rel="stylesheet"/>
-    <link href="css/product.css" rel="stylesheet"/>
+<label style=""><select style="width: 35em;" class="form-control" name="sub_cat" id="subcategory">
+<option>Select Mid Category First</option>
+</select></label><br>
 
-  <style>
-      .noLeftPadding {
-          padding-left: 0 !important;
+<label style=""><select style="width: 35em;" class="form-control" name="sub_sub_cat" id="subsubcategory">
+<option>Select SubCategory First</option>
+</select></label><br>
+
+<label style=""><input style=""  type="file" name="img1"></label><br>
+
+<label style=""><input  style=""  type="file" name="img2"></label><br>
+
+<label style=""><input  style=";"  type="file" name="img3"></label><br>
+
+<label style=""><input  style=""  type="file" name="img4"></label><br>
+
+<label style=""><input   style="" type="file" name="img5"></label><br>
+
+<label style=""><input style="width: 35em;" class="form-control" type="text" name="keywords"></label><br>
+
+<label style=""><!--<input  type="color" name="color"><input class="jscolor {required:false}"  type="hidden" >  -->
+<select style="width:35em;" class="form-control" name="color" >
+<option value="" style="width: 35em;">Select Color</option>
+<option value="Red" id="one" style="background:#FF0000">Red</option>
+<option value="Blue" id="three" style="background:  #1E90FF">Blue</option>
+<option value="Orange" id="five" style="background: #FF4500">Orange</option>
+<option value="Green" id="four" style="background:#008000">Green</option>
+<option value="Yellow" id="two" style="background:#FFFF00">Yellow</option>
+<option value="Purple" id="eight" style="background:#800080">Purple</option>
+<option value="Pink" id="nine" style="background:#ff69b4">Pink</option>
+<option value="Gray" id="eleven" style="background:#808080">Gray</option>
+<option value="Brown" id="six" style="background: #8B4513">Brown</option>
+<option value="White" id="ten" style="background:white">White</option>
+<option value="Black" id="seven" style="background:black">Black</option>
+<option value="Magenta" id="twelve" style="background:#660033">Magenta</option>
+<option value="Maroon" id="fourteen" style="background:#800000">Maroon</option>
+<option value="Peach" id="thirteen" style="background:#FF8362 ">Peach</option>
+<option value="Rust" id="fourteen" style="background:#b7410e">Rust</option>    
+</select></label><br>
+
+<label style=""><select style="width:35em;" class="form-control" name="size">
+<?php 
+ echo view_all_sizes();
+?>
+</select></label><br>
+<label>
+
+<label style=""><input style="width: 35em;" class="form-control" type="text" name="price" required></label><br>
+
+
+<label style=""><input style="width: 35em;" class="form-control" type="text" name="discount"></label><br>
+
+<label style="">
+<select  style="width:35em;" class="form-control" name="stock_availability" >
+<?php 
+$i=1;
+for($i=1;$i<=10;$i++)
+{
+    ?>
+<option value="<?php echo $i;?>"><?php echo $i;?></option> 
+<?php
+}
+?>
+</select>
+</label ><br>
+
+<label style=""><select style="width:35em;" class="form-control" name="product_priority">
+<option value="High">High</option>
+<option value="Medium">Medium</option>
+<option value="Low">Low</option>
+</select></label><br>
+
+</div>
+</form>
+</div>
+
+
+<table style="border-top:1px solid #A9A9A9;margin-top:1%">
+</table><br>
+
+
+
+
+
+
+
+
+
+
+<center><button  name="add_product">Add product</button>
+</center>
+</form>
+</div>
+<?php 
+include 'inc/database.php';
+if(isset($_POST['add_product']))
+{
+    
+    
+    $product_name=$_POST['product_name'];
+    $product_id=$_POST['product_id'];
+    $cat_id=$_POST['main_cat'];
+    $sub_cat_id=$_POST['sub_cat'];
+    $sub_sub_cat_id=$_POST['sub_sub_cat'];
+    $product_desc=$_POST['product_description'];
+    $product_img1 = $_FILES['img1']['name'];
+    $product_img1_tmp =$_FILES['img1']['tmp_name'];
+    
+    $product_img2 = $_FILES['img2']['name'];
+    $product_img2_tmp =$_FILES['img2']['tmp_name'];
+    
+    $product_img3 = $_FILES['img3']['name'];
+    $product_img3_tmp =$_FILES['img3']['tmp_name'];
+    
+    $product_img4 = $_FILES['img4']['name'];
+    $product_img4_tmp =$_FILES['img4']['tmp_name'];
+    
+    $product_img5 = $_FILES['img5']['name'];
+    $product_img5_tmp =$_FILES['img5']['tmp_name'];
+    $product_color=$_POST['color'];
+    $product_size=$_POST['size'];
+    $product_price=$_POST['price'];
+    $keywords=$_POST['keywords'];
+    $discount=$_POST['discount'];
+    $product_stock_avail=$_POST['stock_availability'];
+    $product_prority=$_POST['product_priority'];
+    date_default_timezone_set("Asia/Calcutta");
+    $date=date("H:i:s");
+    move_uploaded_file($product_img1_tmp,"../images/product_images/$product_img1");
+    move_uploaded_file($product_img2_tmp,"../images/product_images/$product_img2");
+    move_uploaded_file($product_img3_tmp,"../images/product_images/$product_img3");
+    move_uploaded_file($product_img4_tmp,"../images/product_images/$product_img4");
+    move_uploaded_file($product_img5_tmp,"../images/product_images/$product_img5");
+    
+    
+    if(empty($product_name))
+    {
+        echo "<script>alert('Please Enter Product Name')</script>";
+    }
+    else{
+        if(empty($product_id)){
+            echo "<script>alert('Please Enter Product Id')</script>";
+        }
+    else
+    {
+        if(empty($product_color))
+        {
+            echo "<script>alert('Please Enter Product Color')</script>";
+        }
+      else
+      {
+          if(empty($product_size))
+          {
+              echo "<script>alert('Please Enter Product Size')</script>";
+          }
+
+          else
+          {
+              if(empty($product_price))
+              {
+                  echo "<script>alert('Please Enter Product Price')</script>";
+              }
+              else 
+              {
+                  if(empty($product_stock_avail))
+                  {
+                      echo "<script>alert('Please Enter Product Stock Availability')</script>";
+                  }
+                  else 
+                  {
+                      $pattern = '/^\d+(?:\.\d{2})?$/';
+                      
+                      if (preg_match($pattern, $_POST['price']) == '0') {
+                          echo "<script>alert('Please Enter Valid Price')</script>";
+                          exit;
+                      }
+                      else {
+                          $add_products=$conn->prepare("insert into products(product_id, product_name, cat_id, sub_cat_id,sub_sub_cat_id, product_desc,
+product_img1, product_img2, product_img3, product_img4, product_img5, product_color, product_size, product_price,keywords,
+Product_stock_avail, product_priroty,product_discount)
+ values('$product_id','$product_name','$cat_id','$sub_cat_id','$sub_sub_cat_id','$product_desc','$product_img1','$product_img2','$product_img3','$product_img4',
+'$product_img5','$product_color','$product_size','$product_price','$keywords','$product_stock_avail','$product_prority','$discount')");
+                          if($add_products->execute())
+                          {
+                              echo "<script>alert('Product added Successfully');
+                             
+                                   window.location='index.php?viewall_products'</script>";
+                          }
+                          else
+                          {
+                              echo "<script>alert('Product Not added Successfully')</script>";
+                          }
+                      }
+          
+                  }
+              }
+          }
       }
-   @media (min-width: 768px)
-navbar.less:385
-.navbar-right {
-    float: right!important;
-    margin-right: -15px;
-    margin-top: -72px;
-}
-@media only screen and (max-width: 1920px) {
-.col-sm-3{
-width:22%;
-}
-}
-.dropdown-menu>li>a {
-    display: block;
-    padding: 0px 0px;
-    clear: both;
-    font-weight: 400;
-    line-height: 1.42857143;
-    color: #333;
-    white-space: nowrap;
+ }
+    }
+    
 }
 
-   .content-wrapper {
-       min-height: 650px;
-       margin: 1em 0;
-   }
-
-  </style>
-</head>
+?>
+<style>
+button
+{
 
 
-<?php include"includes/header.php"; ?>
-<div class="container-fluid content-wrapper" style="margin-top:5em;">
-    <div class="row">
-        <div class="col-sm-2 noLeftPadding">
-            <div class="sidebar">
-                <h2>Categories</h2>
-                <div class="btn-group" role="group">
-                   <a data-toggle="tab" href="#women"> <button class="women" id="wom_btn" type="button" class="btn btn-default categories active"> 
-                        Women
-                    </button> </a>
-                   <a class="kids" data-toggle="tab" href="#kids"> <button type="button" id="kid_btn" class="btn btn-default">
-                        Kids
-                    </button> </a>
-                
-               <div class="tab-content">
-                <?php getMainCart() ?>
-               </div></div>
-                    <h4>Filter by color</h4>
-                    <ul class="filter_color">
-                    <?php getColor(); ?>
-                    </ul>
+    float: left;
+    margin-top: 1%;
+    margin-left: 30%;
+    color: white;
+    text-decoration: none;
+    font-weight: 15px;
+    background: #202020;
+    padding: 10px;
+    border-radius: 5px;
+}
 
 
-
-                    <h4>Filter by size</h4>
-                    <ul class="filter_size">
-                        <?php getSize(); ?>
-                    </ul>
-
-                <h4>Filter by price</h4>
-                <form class="form-group">
-                <input type="text" placeholder="max" style="width: 37px;padding: 2px;;">
-                <input type="text" placeholder="min"  style="width: 37px;padding: 2px;">
-                <input type="submit" style="width:65px;">
-                </form>
-            </div>
-        </div>
-
-
-        <div class="col-sm-9">
-            <div id="products_box">
-                <?php
-                    $sql = "select * from products p join sub_cat s on p.sub_cat_id = s.sub_cat_id where product_priroty='High' LIMIT 0,9";
-                    $query = mysql_query($sql);
-                    while ($row_pro = mysql_fetch_array($query)) {
-                        $pro_id = $row_pro['product_id'];
-                        $pro_img1 = $row_pro['product_img1'];
-                        $pro_name = $row_pro['product_name'];
-                        $sub_cat_name = $row_pro['sub_cat_name'];
-                        $pro_price = $row_pro['product_price'];
-
-                        echo "
-    <div class='product-item col-sm-3'>
-       <a href='details.php?id=$pro_id'>
-            <img src='admin_area/product_images/$pro_img1' width='180' height='180' />
-       </a>
-          <h4 class='details_name'>$pro_name</h4><a href='wishlist.php?id=$pro_id'><i class='fa fa-heart-o pull-right' aria-hidden='true' style='margin-top: -30px;margin-right: 5px;'></i></a>
-            <p  class='details_name'>$sub_cat_name/<b> Price: <i class='fa fa-inr' aria-hidden='true'></i> $pro_price </b></a><br>
-                <div class='btn-group btn-group-justified product_det_btn'>
-                            <a href='details.php?id=$pro_id' class='btn btn-primary'>View</a>
-                            <a class='btn btn-primary' href='check_cart.php?id=$pro_id'>Add To Cart</a>
-                </div>
-      </div>
-    ";
-                    
-                }
-                ?>
-            </div>
-        </div>
-    </div>
-    </div>
-
-
-
-<!-- Footer -->
-<?php include "includes/footer.php"; ?>
-
-<script>
-$(document).ready(function(){
-  // Initialize Tooltip
-  $('[data-toggle="tooltip"]').tooltip(); 
+button:hover {
+  background: white;
+color:black;
   
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+}
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-
-  // Product Loop Select Items
-   
-$('#wom_btn').click(function(){
-          $('#').show();
-          $('#').hide();
-        });
-
-
-
-});
-</script>
-
-
-
-<script>
- $('.main-category .title-category').each(function() {
-	$(this).on('click',function() {
-		var titleCatId = $(this).attr('subCat');
-		console.log(titleCatId);
-		var angleUpDown = $(this).find('.cart').children('i').hasClass('fa-angle-up');
-		$(this).siblings('div.showdetail-'+titleCatId).addClass('show');
-		$(this).siblings('div.showdetail-'+titleCatId).removeClass('hide');
-		$(this).find('.cart').children('i').removeClass('fa-angle-up');
-		$(this).find('.cart').children('i').addClass('fa-angle-down');
-		console.log($(this).find('.cart').children('i').hasClass('fa-angle-up'));
-		console.log($(this).find('.cart').children('i').hasClass('fa-angle-down'));
-		if(angleUpDown) {
-		   $(this).siblings('div.showdetail-'+titleCatId).addClass('hide');
-		   $(this).siblings('div.showdetail-'+titleCatId).removeClass('show');
-		   $(this).find('.cart').children('i').removeClass('fa-angle-up');
-		   $(this).find('.cart').children('i').addClass('fa-angle-down');
-		} else {
-		   $(this).siblings('div.showdetail-'+titleCatId).addClass('show');
-		   $(this).siblings('div.showdetail-'+titleCatId).removeClass('hide');
-		   $(this).find('.cart').children('i').addClass('fa-angle-up');
-		   $(this).find('.cart').children('i').removeClass('fa-angle-down');
-		}
-	});
-});
-/*
-$('.women').each(function(){
-	$('.women').on('click',function() {
-        var sub = $('.title-category').attr('mainCat');
-        console.log(sub);
-        if(sub==24){
-        console.log("hi");
-$('.title-')
-
-         $('.title-category').find('subCat').removeClass('show'); 
-        }
-        else{
-        console.log("hello");  
-        }
-    });
-});
-
-*/
-
-	$('.women').on('click',function() {
-           $('.main-category').each(function(i,obj){
-        if(obj.class == 'title-category' && obj.mainCat == 24){
-var ob = $('.title-category').attr('mainCat');
-} 
-        console.log(ob);
-});
-
-});
-
-</script>
+@media only screen and (min-width: 1920px) {
+bodyright .sel_cat_name{
+padding-left:10.5%;
+}
+}
+</style>
